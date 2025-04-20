3e6b8304521d3a31b3c8e833e685634b62389340
s s <41350108-sarah19042006@users.noreply.replit.com>
2025-04-20 22:21:01 +0000

    ajout des fichier depuis replit

---
+++ piece.c
@@ -0,0 +1,13 @@
+#include "piece.h"
+
+void lire_piece(char* nom_fichier,Piece* piece){
+  FILE* fichier= fopen ("style_piece.txt","r");
+  if (fichier==NULL){
+    printf("erreur dans la lecture du fichier\n");
+  return;
+  }
+}
+fget(fichier,piece->nom,taille_nom);
+for(int i=0; i<taille_piece,i++){
+  fget(fichier,piece->forme[i],taille_piece);
+} 
---
+++ piece.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define taille_piece 5
+#define taille_nom 50
+
+typedef struct{
+char forme  [taille_piece][taille_piece];
+char nom [taille_nom];
+}Piece; 
+
