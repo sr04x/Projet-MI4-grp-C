8a55779e0b1f5afc5b88fb5fcaaf5c1ca785ec4d
yanisgit <naymeyanis@cy-tech.fr>
2025-05-02 11:04:15 +0200

    Update main.c

--- main.c
+++ main.c
@@ -11,5 +11,14 @@ int main() {
     if (nbLigneSuppr > 0) {
         printf("%d ligne(s) supprimée(s) !\n", nbLigneSuppr);
     }
+    score += nbLigneSuppr * 100; //100 points par ligne supprime
+
+    char nom[NOM_MAX];
+    printf("Entrez votre nom :");
+    scanf("%s", nom); //ajouter securiter scanf
+
+    ajouterScore(nom,score);
+    afficherScore();
+    
     return 0;
 }
