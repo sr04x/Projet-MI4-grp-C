66221e2dfedb447b24a7e0dcca46984a42a8058b
yanisgit <naymeyanis@cy-tech.fr>
2025-05-02 11:22:59 +0200

    Update grille.h

--- grille.h
+++ grille.h
@@ -5,7 +5,7 @@
 #define NB_COLONNES 10
 
 #define VIDE ' '
-#define BLOC '@'  // Utilisé pour les blocs affichés dans la grille
+//#define BLOC '@'  // Utilisé pour les blocs affichés dans la grille à changer avec les nouvelles pièces
 
 void initialiserGrille(char grille[NB_LIGNES][NB_COLONNES]);
 void afficherGrille(char grille[NB_LIGNES][NB_COLONNES]);
