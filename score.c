4b6ee275a1c90d14773670ba1bb1cecb1b22ced7
yanisgit <naymeyanis@cy-tech.fr>
2025-05-06 04:03:12 +0200

    Update score.c

--- score.c
+++ score.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 //#include <stlib.h> // pas besoin pour l'instant peut etre plus tard pour trier score (qsort)
 #include <string.h> //pour manipuler chaine de caractere (strcmp() trie score par nom)
-#include score.h
+#include "score.h"
 
 
-void ajouterScores(const char *nom,int score){ //ajout du nom et score dans un fichier
-    FILE *f = fopen(FICHIER_SCORES,"a");
-    if (f = NULL){
-        perror("Erreur lors de l'ouverture du ficher score")
+void afficherScores() {
+    FILE *f = fopen("scores.txt", "r");
+    if (f == NULL) {
+        printf("Aucun score enregistré pour le moment.\n");
+        return;
     }
-    fprintf(f,"%s %d \n", nom, score);
+
+    char ligne[100];
+    printf("\n--- Scores enregistrés ---\n");
+    while (fgets(ligne, sizeof(ligne), f)) {
+        printf("%s", ligne);
+    }
     fclose(f);
 }
 
-void afficherScores(){
-    FILE *f = fopen(FICHIER_SCORES,"r");
-    if (f = NULL){
-        perror("Erreur lors de l'ouverture du ficher score");
-        printf("ou aucun score enregistré pour l'instant\n");
+
+void sauvegarderScore(const char *nom, int score) {
+    FILE *f = fopen("scores.txt", "a");
+    if (f == NULL) {
+        perror("Erreur lors de l'ouverture de scores.txt");
+        return;
     }
-    char nom[NOM_MAX];
-    int score;
-    printf("\n ----- HIGH SCORES -----\n");
-    while(fscanf(f,"%s %d",nom, &score)==2){
-        printf("%s : %d\n",nom, score);
-    }
+    fprintf(f, "%s %d\n", nom, score);
     fclose(f);
 }
